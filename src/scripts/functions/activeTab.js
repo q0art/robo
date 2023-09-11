87d04039f4cb57af0290e28d565301bc350d4fb8
arrowls <babuyoff@gmail.com>
2023-09-11 10:10:14 +0300

    comments

--- src/scripts/functions/activeTab.js
+++ src/scripts/functions/activeTab.js
@@ -1,12 +1,14 @@
+// плохая идея называть функцию как переменную
 export const activeTab = () => {
 	const tabs = document.querySelectorAll('.trainers-tabs__nav-btn');
 	const tabContents = document.querySelectorAll('.trainers-tabs__wrapper');
-
+	// forEach->addEventListener это очень плохо, лучше на документ повесь
 	tabs.forEach((tab, index) => {
 		tab.addEventListener('click', () => {
 			tabContents.forEach((content) => {
 				content.classList.remove('is-active');
 			});
+
 			tabs.forEach((tab) => {
 				tab.classList.remove('is-active');
 			});
@@ -14,6 +16,12 @@ export const activeTab = () => {
 			tabContents[index].classList.add('is-active');
 			tab.classList.add('is-active');
 		});
+	});
+
+	document.addEventListener('click', ({ target }) => {
+		const activeTab = target.closest('[data-tab]'); // или по классам, как угодно
+		if (activeTab) return;
+		// code
 	});
 
 	// delete maybe
